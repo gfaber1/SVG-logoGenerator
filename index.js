7ddc2fb0a57f5eba1491272e09f8a290661578ac
Gabe Faber <gmfaber55@gmail.com>
2023-02-22 11:34:38 -0700

    wrote the questions, moved the max input inquirer const to the index.js file

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const inquirer = require('inquirer')
 const fs = require('fs');
 const shapes = require('./lib/shapes')
 const questions = require('./lib/questions')
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
+
+inquirer.registerPrompt('maxLength-input', MaxLengthInputPrompt)
+
+inquirer
+    .Prompt(questions)
+    .then(() => {
+
+    })
