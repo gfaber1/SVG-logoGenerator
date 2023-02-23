dba9fc6fe1e2c22176202f31f5aa1f3457de9ad8
Gabe Faber <gmfaber55@gmail.com>
2023-02-23 11:49:18 -0700

    created a function for the inquirer in the index file. changed wording on the first question

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const inquirer = require('inquirer')
 const fs = require('fs');
 const shapes = require('./lib/shapes')
 const questions = require('./lib/questions')
-const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 
-inquirer.registerPrompt('maxLength-input', MaxLengthInputPrompt)
 
-inquirer
-    .Prompt(questions)
-    .then(() => {
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(function (data) {
+            writeToFile('logo.svg', logoGenerator(data));
+        });
+}
 
-    })
+// Call initialize function
+init();
