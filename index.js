c701258ab83085b08df11349afa14e5d6103ce1b
Gabe Faber <gmfaber55@gmail.com>
2023-03-02 11:36:06 -0700

    did a lot of work on this commit. created an examples folder and a logo.svg file. finalized my shapes.js file. wrote tests for my shapes. created and wrote an svg.js file to deal will created the svg and the svg text areas.added correct dependencies for my index.js file, finished writing the .then for inquirer and wrote to the svg file.

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
+
 const inquirer = require('inquirer')
-const fs = require('fs');
-const shapes = require('./lib/shapes')
+const { writeFile } = require('fs/promises');
+
 const questions = require('./lib/questions')
+const SVG = require('./lib/svg')
+const { Circle, Triangle, Square } = require('./lib/shapes')
+
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
-
 
 function init() {
     inquirer
         .prompt(questions)
-        .then(function (data) {
-            writeToFile('logo.svg', logoGenerator(data));
+        .then((answers) => {
+
+            const svg = new SVG()
+            const newCircle = new Circle()
+            const newTriangle = new Triangle()
+            const newSquare = new Square()
+
+            function createSVG(answers) {
+                if (answers.shape === 'circle') {
+                    svg.setTitle(answers.title, answers.textColor)
+                    newCircle.setColor(answers.shapeColor)
+                    svg.setShape(newCircle)
+                    return svg.render()
+                } else if (answers.shape === 'triangle') {
+                    svg.setTitle(answers.title, answers.textColor)
+                    newTriangle.setColor(answers.shapeColor)
+                    svg.setShape(newTriangle)
+                    return svg.render()
+                } else if (answers.shape === 'square') {
+                    svg.setTitle(answers.title, answers.textColor)
+                    newSquare.setColor(answers.shapeColor)
+                    svg.setShape(newSquare)
+                    return svg.render()
+                }
+            }
+
+
+
+            writeFile(`./examples/logo.svg`, createSVG(answers), (err) => {
+                console.log("Generated logo.svg");
+                if (err) throw err;
+                console.log("Generated logo.svg");
+            })
+
+
+
         });
+
+
 }
 
-// Call initialize function
+// Calls the init function
 init();
